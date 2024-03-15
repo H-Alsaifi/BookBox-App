7281601efc3c17d67aa4ae8a70093d43f1dea67b
mohammed <mshalqassabi@gmail.com>
2024-03-15 17:25:46 -0300

    ResetPassword now works + changed it to use bindings instead of dealing with the views directly through xml IDs

--- Application/app/src/main/java/ca/dal/cs/csci4176/group01undergraduate/ResetPasswordActivity.kt
+++ Application/app/src/main/java/ca/dal/cs/csci4176/group01undergraduate/ResetPasswordActivity.kt
@@ -1,27 +1,25 @@
 package ca.dal.cs.csci4176.group01undergraduate
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import ca.dal.cs.csci4176.group01undergraduate.databinding.ActivityResetPasswordBinding
 import com.google.firebase.auth.FirebaseAuth
 
-class ResetPassword : AppCompatActivity() {
+class ResetPasswordActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityResetPasswordBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reset_password)
+        binding = ActivityResetPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
 
-        val emailEditText = findViewById<EditText>(R.id.emailEditText)
-        val submitResetButton = findViewById<Button>(R.id.submitResetButton)
-
-        submitResetButton.setOnClickListener {
-            val email = emailEditText.text.toString().trim()
+        binding.submitResetButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString().trim()
             if (email.isNotEmpty()) {
                 auth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
                     if (task.isSuccessful) {
