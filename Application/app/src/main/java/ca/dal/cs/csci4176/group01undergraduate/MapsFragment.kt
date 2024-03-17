760c14d1b28f73fdb613edec7ee37fd0500130d8
almaimani <mh843120@dal.ca>
2024-03-17 20:57:40 -0300

    Added files that results in conflict

--- Application/app/src/main/java/ca/dal/cs/csci4176/group01undergraduate/MapsFragment.kt
+++ Application/app/src/main/java/ca/dal/cs/csci4176/group01undergraduate/MapsFragment.kt
@@ -11,18 +11,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
-import androidx.compose.material3.BottomSheetDefaults
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.GoogleMap.OnMapLoadedCallback
 import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
 
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.AdvancedMarker
-import com.google.android.gms.maps.model.AdvancedMarkerOptions
-import com.google.android.gms.maps.model.BitmapDescriptor
+
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
